7a8cbda297e6659f5344cf2e67e1d2b1536f08e5
orpheeh <norpheehounkponou@gmail.com>
2020-08-04 18:33:54 +0100

    feat

--- server.js
+++ server.js
@@ -2,18 +2,39 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+class User {
+  constructor(nom, id){
+     this.nom = nom;
+     this.id = id;
+  }
+}
+
+const sockets = [];
+
+let countConnection = 0;
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
 io.on('connection', (socket) => {
+  countConnection++;
   console.log("connexion receive ");
+  sockets.push(new User(countConnection, socket.id));
   socket.on('disconnect', () => {
      console.log("logout");
   });
   socket.on('chat message', (msg) => {
+    console.log(socket.id);
+    const splits = msg.split(":");
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    const user = sockets.find(u => u.nom == splits[0]);
+    console.log(sockets);
+    if(user){
+      console.log(user.id);
+      io.to(user.id).emit('chat message', splits[1]); 
+    }
+
   });
 });
 
